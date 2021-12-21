79228d70bb160480db36c46571afa33f1c4622e1
Student <rosefiero@gmail.com>
2021-12-21 08:21:33 -0800

    QnA Maker completed!

--- 12-qna/ask-question.cmd
+++ 12-qna/ask-question.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variables
-set prediction_url="YOUR_PREDICTION_ENDPOINT"
-set key="YOUR_KEY"
+set prediction_url="https://naheeds-language-service.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=LearnFAQ&api-version=2021-10-01&deploymentName=production"
+set key="3a431b3f01254611875bc37ea00ba546"
 
 curl -X POST !prediction_url! -H "Ocp-Apim-Subscription-Key: !key!" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
